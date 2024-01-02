88f56038ab21430c52ce7439ad0f8ba42d6e28f4
nearworld <artemismars2@outlook.com>
2024-01-02 21:36:59 +0900

    modal on/off feature

--- src/lib/AddTask.svelte
+++ src/lib/AddTask.svelte
@@ -1,14 +1,16 @@
 <script>
 	import Modal from '$lib/Modal.svelte';
 	import Icon from '@iconify/svelte';
+
+	let showModal = false;
 </script>
 
 <div>
-	<button class="btn btn-primary w-full">
+	<button class="btn btn-primary w-full" on:click={() => (showModal = true)}>
 		Add new Task <Icon icon="mdi:plus" />
 	</button>
 
-	<Modal>
+	<Modal bind:showModal>
 		<form>
 			<h3 class="font-bold text-lg">Add new task</h3>
 			<div class="modal-action">
---
+++ src/lib/Modal.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let showModal;
+</script>
+
+<dialog id="my_modal_3" class={`modal ${showModal ? 'modal-open' : ''}`}>
+	<div class="modal-box">
+		<form method="dialog">
+			<button
+				on:click={() => (showModal = false)}
+				class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button
+			>
+		</form>
+		<slot />
+	</div>
+</dialog>
