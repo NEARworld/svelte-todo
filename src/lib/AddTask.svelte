9c2322115a8026bb6fe0c0db6df858aa41aac644
nearworld <artemismars2@outlook.com>
2024-01-02 23:42:12 +0900

    submitting form to add Todo

--- src/lib/AddTask.svelte
+++ src/lib/AddTask.svelte
@@ -1,21 +1,41 @@
 <script>
 	import Modal from '$lib/Modal.svelte';
 	import Icon from '@iconify/svelte';
+	import { invalidate } from '$app/navigation';
 
 	let showModal = false;
+
+	let newTaskValue = '';
+
+	async function handleNewTaskSubmit(e) {
+		e.preventDefault();
+
+		const res = await fetch('/', {
+			method: 'POST',
+			body: JSON.stringify({ id: 2, text: newTaskValue })
+		});
+		const data = await res.json();
+		if (data) {
+			invalidate(() => true);
+		}
+	}
 </script>
 
 <div>
 	<button class="btn btn-primary w-full" on:click={() => (showModal = true)}>
 		Add new Task <Icon icon="mdi:plus" />
 	</button>
-
 	<Modal bind:showModal>
-		<form>
+		<form on:submit={handleNewTaskSubmit}>
 			<h3 class="font-bold text-lg">Add new task</h3>
 			<div class="modal-action">
-				<input type="text" placeholder="Type here" class="input input-bordered w-full" />
-				<button class="btn btn-primary">submit</button>
+				<input
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered w-full"
+					on:input={(e) => (newTaskValue = e.target.value)}
+				/>
+				<button class="btn btn-primary" on:click={() => (showModal = false)}>submit</button>
 			</div>
 		</form>
 	</Modal>
