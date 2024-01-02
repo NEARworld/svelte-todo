820ea78c9c79f3e12cd0bb61281a7617f69f3600
nearworld <artemismars2@outlook.com>
2024-01-02 21:22:46 +0900

    todo list

---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,18 @@
+<div class="overflow-x-auto">
+	<table class="table">
+		<thead>
+			<tr>
+				<th>id</th>
+				<th>text</th>
+				<th>actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>1</th>
+				<td>Cy Ganderton</td>
+				<td>Quality Control Specialist</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
