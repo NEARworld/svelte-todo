53c44a62ee36b4dec77dc613c7e93499de56ca1a
nearworld <artemismars2@outlook.com>
2024-01-03 22:25:39 +0900

    installed @prisma/client

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,4 +1,11 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 export async function load({ fetch }) {
-	const res = await fetch('http://localhost:3001/tasks');
-	return { tasks: await res.json() };
+	const tasks = await prisma.todo.findMany();
+
+	// const data = await prisma.todo.findMany();
+	// const res = await fetch('http://localhost:3001/tasks');
+	return { tasks };
 }
