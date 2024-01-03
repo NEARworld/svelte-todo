41fa6a822d5b204ea295f31c2e6384cb0d06e19b
nearworld <artemismars2@outlook.com>
2024-01-03 10:14:31 +0900

    edit and trash icons on Task

--- src/lib/Task.svelte
+++ src/lib/Task.svelte
@@ -1,9 +1,14 @@
 <script>
+	import Icon from '@iconify/svelte';
+
 	export let task;
 </script>
 
 <tr>
 	<th>{task.id}</th>
 	<td>{task.text}</td>
-	<td>Quality Control Specialist</td>
+	<td class="flex gap-3">
+		<Icon icon="bx:edit" class="scale-150 cursor-pointer text-blue-600" />
+		<Icon icon="mdi:trash-outline" class="scale-175 cursor-pointer text-red-600" />
+	</td>
 </tr>
