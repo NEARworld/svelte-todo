70145f777ce8c4036477b0ee46c09882d48daca0
nearworld <artemismars2@outlook.com>
2024-01-03 11:13:08 +0900

    patch todo api

--- src/routes/+server.js
+++ src/routes/+server.js
@@ -32,3 +32,37 @@ export async function POST({ request }) {
 		);
 	}
 }
+
+export async function PATCH({ request }) {
+	try {
+		const data = await request.json();
+		const res = await fetch(`http://localhost:3001/tasks/${data.id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ text: data.text })
+		});
+		const todo = await res.json();
+
+		return new Response(JSON.stringify({ todo }), {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	} catch (e) {
+		return new Response(
+			JSON.stringify({
+				message: 'Something went wrong while editing Todo',
+				error: e
+			}),
+			{
+				status: 500,
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
+	}
+}
