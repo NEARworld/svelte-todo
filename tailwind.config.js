c9b177cdadc0ff0ad4c68ba189a57e9002172dbc
nearworld <artemismars2@outlook.com>
2024-01-03 10:13:59 +0900

    set a custom tailwindcss scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			scale: {
+				175: '1.75'
+			}
+		}
 	},
 	plugins: [require('daisyui')]
 };
