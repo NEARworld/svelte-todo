8437c77bd2a3312ddae224ee57afcdc381abe77f
nearworld <artemismars2@outlook.com>
2024-01-03 10:39:30 +0900

    empty input value when Modal is closed

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -1,12 +1,18 @@
 <script>
 	export let showModal;
+	export let inputValue;
+
+	function handleCloseModal() {
+		showModal = false;
+		inputValue = '';
+	}
 </script>
 
 <dialog id="my_modal_3" class={`modal ${showModal ? 'modal-open' : ''}`}>
 	<div class="modal-box">
 		<form method="dialog">
 			<button
-				on:click={() => (showModal = false)}
+				on:click={handleCloseModal}
 				class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button
 			>
 		</form>
